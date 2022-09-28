b202bd60f0ee375ac25adb0e1d5a622a22201feb
junseongwoo <wnstjd4852@gmail.com>
2022-09-28 13:09:47 +0900

    오델로 게임

---
+++ othello/main.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace othello
+{
+    partial class frm_main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox_othello = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_othello)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox_othello
+            // 
+            this.picBox_othello.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.picBox_othello.Location = new System.Drawing.Point(28, 81);
+            this.picBox_othello.Name = "picBox_othello";
+            this.picBox_othello.Size = new System.Drawing.Size(652, 494);
+            this.picBox_othello.TabIndex = 0;
+            this.picBox_othello.TabStop = false;
+            this.picBox_othello.Paint += new System.Windows.Forms.PaintEventHandler(this.picBox_othello_Paint);
+            // 
+            // frm_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 587);
+            this.Controls.Add(this.picBox_othello);
+            this.Name = "frm_main";
+            this.Text = "Othello";
+            this.Load += new System.EventHandler(this.frm_main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_othello)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox_othello;
+    }
+}
+
---
+++ othello/main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace othello
+{
+    public partial class frm_main : Form
+    {
+        public frm_main()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_main_Load(object sender, EventArgs e)
+        {
+            picBox_othello.Paint += picBox_othello_default;
+        }
+
+        private void picBox_othello_default(object sender, PaintEventArgs e)
+        {
+            // 오셀로 판 그리기
+            PictureBox pb = (PictureBox)sender;
+            Graphics g = e.Graphics;
+            Pen pen = Pens.Black;
+            int othelloSize = 8; // 8x8 크기 
+
+            for (int i = 1; i < 8; i++)
+            {
+                g.DrawLine(pen, (pb.Width/ othelloSize) *i, 0, (pb.Width/ othelloSize) *i, pb.Height);
+                g.DrawLine(pen, 0, (pb.Height/ othelloSize) * i, pb.Width, (pb.Height/ othelloSize) * i);
+            }
+        }
+
+        private void picBox_othello_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
