2a148fea258c2806ee2f635f00b94110f4ccd1a9
junseongwoo <wnstjd4852@gmail.com>
2022-11-07 17:56:34 +0900

    오델로 게임

--- othello/Logic/Game.cs
+++ othello/Logic/Game.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace othello.Logic
 {
+    // How to separate cells in One picturebox
+    // https://stackoverflow.com/questions/12711843/picturebox-grid-and-selecting-individual-cells-when-clicked-on
+    // 
     class Game
     {
 
--- othello/main.Designer.cs
+++ othello/main.Designer.cs
@@ -31,11 +31,17 @@ namespace othello
         {
             this.btn_Start = new System.Windows.Forms.Button();
             this.btn_ReStart = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_Start
             // 
-            this.btn_Start.Location = new System.Drawing.Point(341, 61);
+            this.btn_Start.Location = new System.Drawing.Point(15, 3);
             this.btn_Start.Name = "btn_Start";
             this.btn_Start.Size = new System.Drawing.Size(83, 29);
             this.btn_Start.TabIndex = 1;
@@ -44,23 +50,58 @@ namespace othello
             // 
             // btn_ReStart
             // 
-            this.btn_ReStart.Location = new System.Drawing.Point(430, 61);
+            this.btn_ReStart.Location = new System.Drawing.Point(15, 38);
             this.btn_ReStart.Name = "btn_ReStart";
             this.btn_ReStart.Size = new System.Drawing.Size(83, 29);
             this.btn_ReStart.TabIndex = 2;
             this.btn_ReStart.Text = "재시작";
             this.btn_ReStart.UseVisualStyleBackColor = true;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.1103F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.88969F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(757, 608);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(108, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(545, 511);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_Start);
+            this.panel1.Controls.Add(this.btn_ReStart);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(99, 347);
+            this.panel1.TabIndex = 1;
+            // 
             // frm_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 608);
-            this.Controls.Add(this.btn_ReStart);
-            this.Controls.Add(this.btn_Start);
+            this.ClientSize = new System.Drawing.Size(757, 608);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "frm_main";
             this.Text = "Othello";
             this.Load += new System.EventHandler(this.frm_main_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -68,6 +109,9 @@ namespace othello
         #endregion
         private System.Windows.Forms.Button btn_Start;
         private System.Windows.Forms.Button btn_ReStart;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
