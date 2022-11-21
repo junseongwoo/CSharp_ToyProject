f09941815d7805e0ae64510776523bc1e5053f7b
jswoo <wnstjd4852@gmail.com>
2022-11-21 23:19:23 +0900

    Omok Game

---
+++ Omok/Omok/Main.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Omok
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.mspMain = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.보기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.수순ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlMain.SuspendLayout();
+            this.mspMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.mspMain);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(800, 450);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // mspMain
+            // 
+            this.mspMain.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.mspMain.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.mspMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.보기ToolStripMenuItem,
+            this.수순ToolStripMenuItem});
+            this.mspMain.Location = new System.Drawing.Point(0, 0);
+            this.mspMain.Name = "mspMain";
+            this.mspMain.Size = new System.Drawing.Size(800, 33);
+            this.mspMain.TabIndex = 0;
+            this.mspMain.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(64, 29);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // 보기ToolStripMenuItem
+            // 
+            this.보기ToolStripMenuItem.Name = "보기ToolStripMenuItem";
+            this.보기ToolStripMenuItem.Size = new System.Drawing.Size(64, 29);
+            this.보기ToolStripMenuItem.Text = "보기";
+            // 
+            // 수순ToolStripMenuItem
+            // 
+            this.수순ToolStripMenuItem.Name = "수순ToolStripMenuItem";
+            this.수순ToolStripMenuItem.Size = new System.Drawing.Size(64, 29);
+            this.수순ToolStripMenuItem.Text = "수순";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlMain);
+            this.MainMenuStrip = this.mspMain;
+            this.Name = "Main";
+            this.Text = "Main";
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            this.mspMain.ResumeLayout(false);
+            this.mspMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.MenuStrip mspMain;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 보기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 수순ToolStripMenuItem;
+    }
+}
+
---
+++ Omok/Omok/Main.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Omok
+{
+    public partial class Main : Form
+    {
+        int margin = 40;
+        int cellSize = 30;
+        int stoneSize = 28;
+        int flowerPointSize = 10;
+
+        Graphics g;
+        Pen pen;
+        Brush wBrush, bBrush;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            this.BackColor = Color.Orange;
+
+            pen = new Pen(Color.Black);
+            bBrush = new SolidBrush(Color.Black);
+            wBrush = new SolidBrush(Color.White);
+
+            this.ClientSize = new Size(2 * margin + 18 * cellSize,
+                2 * margin + 18 * cellSize + mspMain.Height);
+            
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            DrawBoard();
+        }
+
+        private void DrawBoard()
+        {
+            g = pnlMain.CreateGraphics();
+
+            for (int i = 0; i < 19; i++)
+            {
+                // 세로선
+                g.DrawLine(pen, new Point(margin + i * cellSize, margin),
+                    new Point(margin + i * cellSize, margin + 18 * cellSize));
+                // 가로선
+                g.DrawLine(pen, new Point(margin, margin + i * cellSize),
+                    new Point(margin + 18 * cellSize, margin + i * cellSize));
+            }
+
+            // 화점 그리기
+            for (int i = 3 ; i <= 15; i+=6)
+                for (int j = 3; j <= 15; j+=6)
+                {
+                    g.FillRectangle(bBrush,
+                        margin + cellSize * i - flowerPointSize / 2,
+                        margin + cellSize * j - flowerPointSize / 2,
+                        flowerPointSize, flowerPointSize);
+                }
+        }
+    }
+}
