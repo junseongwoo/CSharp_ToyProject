f5e61a71c3c9897221b74b6d905327c24a515327
junseongwoo <wnstjd4852@gmail.com>
2022-12-14 17:58:15 +0900

    EsVision 구현 중

---
+++ ESVision/UI/BottomView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ESVision.UI
+{
+    public partial class BottomView : Form
+    {
+        public BottomView()
+        {
+            InitializeComponent();
+        }
+    }
+}
