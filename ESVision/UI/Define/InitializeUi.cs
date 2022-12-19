8c8065922605b7821422dae2176ec7f3009984f6
junseongwoo <wnstjd4852@gmail.com>
2022-12-19 15:07:29 +0900

    Ui 정의

---
+++ ESVision/UI/Define/InitializeUi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ESVision
+{
+    class InitializeUi
+    {
+        public void CreatUiInsidePanel(Form form, Control panel)
+        {
+            form.TopLevel = false;
+            form.Location = new Point(0, 0);
+            panel.Controls.Add(form);
+        }
+
+        public void ShowUi(Form form)
+        {
+            form.TopLevel = true;
+            form.StartPosition = FormStartPosition.CenterParent;
+            form.Show();
+        }
+    }
+}
---
+++ ESVision/UI/Define/UiDefine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESVision
+{
+    #region [열거형 : Image Menu List]
+    public enum EIMAGE_MENU_LIST
+    {
+        FORM_NEW_IMAGE,
+        FORM_OPEN_IMAGE,
+        FORM_SAVE_IMAGE
+    }
+    #endregion
+
+    #region [열거형 : Image Tool Menum List]
+    public enum EIMAGE_TOOL_MENU_LIST
+    {
+        FORM_SUBTRACTION_IMAGE
+    }
+    #endregion
+}
