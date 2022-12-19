1d39939e4163f12ba171f9dc11a1f4261a30512d
junseongwoo <wnstjd4852@gmail.com>
2022-12-19 15:08:03 +0900

    새로운 이미지 불러오기 Ui 수정

---
+++ ESVision/UI/VisionUtil/formNewImage.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace ESVision
+{
+    partial class formNewImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // formNewImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(610, 484);
+            this.Name = "formNewImage";
+            this.Text = "New Image";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
