20950f42ee2991d944dec7016d23bd67b881c92e
jswoo <wnstjd4852@gmail.com>
2022-12-21 23:49:40 +0900

    Convert Gray Scale

--- ESVision/System/Image/ImgLib.cs
+++ ESVision/System/Image/ImgLib.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -44,13 +45,52 @@ namespace ESVision
         {
             Bitmap bitmapImg = Bitmap.FromFile(imgPath) as Bitmap;
 
+            bitmapImg = RGB2Gray(bitmapImg);
+
             return bitmapImg;
+        }
+        #endregion
+
+        #region [멤버 함수 : Image to Grayscale]
+        public Bitmap RGB2Gray(Bitmap img)
+        {
+            // 속도를 어떻게 늘릴 수 있을까? 
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Bitmap grayImg = new Bitmap(img.Width, img.Height);
+
+            int width = img.Width;
+            int height = img.Height;
+            Color argb;
+            Color gray;
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    argb = img.GetPixel(x, y);
+
+                    int a = argb.A;
+                    int r = argb.R;
+                    int g = argb.G;
+                    int b = argb.B;
+
+                    int avg = (r + g + b) / 3;
+                    gray = Color.FromArgb(a, avg, avg, avg);
+                    grayImg.SetPixel(x, y, gray);
+                }
+            }
+            watch.Stop();
+            TimeSpan time = watch.Elapsed;
+            MessageBox.Show(time.ToString());
+            return grayImg;
         }
         #endregion
 
         #region [멤버 함수 : Image Subtraction] 
         public Bitmap Sub(Bitmap baseImg, Bitmap srcImg)
         {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
             int WbaseImg = baseImg.Width;
             int HbaseImg = baseImg.Height;
 
@@ -62,12 +102,15 @@ namespace ESVision
                 {
                     Color baseColor = baseImg.GetPixel(x, y);
                     Color srcColor = srcImg.GetPixel(x, y);
-                    Color resultColor = Color.FromArgb((baseColor.A - srcColor.A) | (baseColor.R - srcColor.R) | (baseColor.G - srcColor.G) | (baseColor.B - srcColor.B));
+                    // RGB 값이 -가 나온다. 절대 값으로 해보자 
+                    Color resultColor = Color.FromArgb(255 , (baseColor.R - srcColor.R) , (baseColor.G - srcColor.G) , (baseColor.B - srcColor.B));
 
                     resultImg.SetPixel(x, y, resultColor);
                 }
             }
-
+            watch.Stop();
+            TimeSpan time = watch.Elapsed;
+            MessageBox.Show(time.ToString());
             return resultImg;
         }
         #endregion
