32dffd1277ea2585a5b6bc5a2227c5d517aa21db
junseongwoo <wnstjd4852@gmail.com>
2023-01-10 17:27:00 +0900

    좌표 표시 UI 수정

--- ESVision/UI/Frame/BottomView.Designer.cs
+++ ESVision/UI/Frame/BottomView.Designer.cs
@@ -32,6 +32,12 @@ namespace ESVision
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsTxtX = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsTxtY = new System.Windows.Forms.ToolStripTextBox();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,7 +46,13 @@ namespace ESVision
             this.toolStrip1.AutoSize = false;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripSeparator1,
-            this.toolStripLabel1});
+            this.toolStripLabel1,
+            this.toolStripSeparator2,
+            this.tsTxtX,
+            this.toolStripSeparator3,
+            this.toolStripLabel2,
+            this.toolStripSeparator4,
+            this.tsTxtY});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(1900, 25);
@@ -58,6 +70,39 @@ namespace ESVision
             this.toolStripLabel1.Name = "toolStripLabel1";
             this.toolStripLabel1.Size = new System.Drawing.Size(14, 22);
             this.toolStripLabel1.Text = "X";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsTxtX
+            // 
+            this.tsTxtX.Name = "tsTxtX";
+            this.tsTxtX.Size = new System.Drawing.Size(50, 25);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(14, 22);
+            this.toolStripLabel2.Text = "Y";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsTxtY
+            // 
+            this.tsTxtY.Name = "tsTxtY";
+            this.tsTxtY.ReadOnly = true;
+            this.tsTxtY.Size = new System.Drawing.Size(50, 25);
+            this.tsTxtY.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // BottomView
             // 
@@ -80,5 +125,11 @@ namespace ESVision
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripTextBox tsTxtX;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripTextBox tsTxtY;
     }
 }
