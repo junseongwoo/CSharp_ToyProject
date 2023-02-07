83ef6e64c93907d544882a8e8954fbb89f54abda
junseongwoo <wnstjd4852@gmail.com>
2023-02-07 11:30:20 +0900

    파일 관리 클래스 구현 중

---
+++ ESVision/System/FileSystem/FileLib.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESVision
+{
+    class FileLib
+    {
+        // 한달 이전의 파일 지우기
+        public void DeleteFileOneMonthAgo(string directoryPath)
+        {
+            DateTime OneMonthAgo = DateTime.Now.AddMonths(-1);
+
+            foreach (string filePath in Directory.GetFiles(directoryPath))
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+
+                if (fileInfo.LastAccessTime < OneMonthAgo)
+                {
+                    fileInfo.Delete();
+                }
+            }
+        }
+
+        // 지정된 파일 개수 이상의 파일 지우기 : 오래된 순으로 지움
+        public void DeleteFileLimit(string filePath, int limitNum)
+        {
+
+        }
+
+    }
+}
