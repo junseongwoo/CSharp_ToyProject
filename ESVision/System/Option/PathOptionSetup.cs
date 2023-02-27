75b532cdb3fa3c7f8c4dfc05dafb4ebca6c5c719
junseongwoo <wnstjd4852@gmail.com>
2023-02-27 16:09:11 +0900

    Option Ui 수정 중

---
+++ ESVision/System/Option/PathOptionSetup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESVision
+{
+    public class PathOptionSetup
+    {
+        [Category("Test")]
+        public int Test2 { get; set; }
+
+        public PathOptionSetup()
+        {
+
+        }
+        
+    }
+}
