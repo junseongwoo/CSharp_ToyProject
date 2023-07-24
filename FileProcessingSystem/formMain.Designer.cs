bce7d7eb15e00ab83e1df15cbc998481402c0da7
junseongwoo <wnstjd4852@gmail.com>
2023-07-24 17:57:20 +0900

    파일 처리 시스템 구현 중

--- FileProcessingSystem/formMain.Designer.cs
+++ FileProcessingSystem/formMain.Designer.cs
@@ -29,10 +29,17 @@ namespace FileProcessingSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormMain";
+            this.Name = "FormMain";
+            this.Text = "File Management System";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
